5d1c2fc6c371b4d80fa2f89c30c06b7e4de8b02a
Evan Feenstra <evanfeenstra@gmail.com>
2022-08-08 11:55:23 -0700

    recover, verify_within, tests

--- auther/src/lib.rs
+++ auther/src/lib.rs
@@ -1,28 +1,28 @@
 use anyhow::anyhow;
 use anyhow::Result;
 use base64::{decode_config, encode_config, URL_SAFE};
-use secp256k1::ecdsa::Signature;
+use secp256k1::ecdsa::{self, Signature};
 use secp256k1::hashes::sha256d::Hash as Sha256dHash;
 use secp256k1::hashes::Hash;
 use secp256k1::{Message, PublicKey, Secp256k1, SecretKey};
 use std::convert::TryInto;
 
-pub struct Token(u64);
+pub struct Token(u32, Option<[u8; 65]>);
 
-fn now() -> u64 {
+fn now() -> u32 {
     use std::time::{SystemTime, UNIX_EPOCH};
     let start = SystemTime::now();
     let since_the_epoch = start
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards");
-    since_the_epoch.as_nanos() as u64
+    since_the_epoch.as_secs() as u32
 }
 
-pub fn u64_to_bytes(input: u64) -> [u8; 8] {
+pub fn u32_to_bytes(input: u32) -> [u8; 4] {
     input.to_le_bytes()
 }
-pub fn bytes_to_u64(bytes: [u8; 8]) -> u64 {
-    u64::from_le_bytes(bytes)
+pub fn bytes_to_u32(bytes: [u8; 4]) -> u32 {
+    u32::from_le_bytes(bytes)
 }
 
 pub fn base64_encode(input: &Vec<u8>) -> String {
@@ -34,39 +34,62 @@ pub fn base64_decode(input: &str) -> Result<Vec<u8>> {
 }
 
 impl Token {
+    /// Creates a new token with current timestamp
     pub fn new() -> Self {
-        Self(now())
+        Self(now(), None)
     }
-    pub fn from_bytes(bytes: [u8; 8]) -> Self {
-        Self(bytes_to_u64(bytes))
+    pub fn set_sig(&mut self, sig: [u8; 65]) {
+        self.1 = Some(sig)
     }
     pub fn from_base64(s: &str) -> Result<Self> {
         if s.len() < 8 {
             return Err(anyhow!("too short slice".to_string()));
         }
         let bytes = base64_decode(s)?;
-        let ts: [u8; 8] = bytes[..8].try_into()?;
-        Ok(Self(bytes_to_u64(ts)))
+        let ts: [u8; 4] = bytes[..4].try_into()?;
+        let sig: [u8; 65] = bytes[4..].try_into()?;
+        Ok(Self(bytes_to_u32(ts), Some(sig)))
     }
     pub fn expected_len(&self) -> usize {
-        73
+        69
     }
+    /// Sign a lightning token
     pub fn sign(&self, secret_key: &SecretKey) -> Result<Vec<u8>> {
-        let mut ts = u64_to_bytes(self.0).to_vec();
+        let mut ts = u32_to_bytes(self.0).to_vec();
         let sig = self.sign_message(&ts, secret_key)?;
         println!("tts {:?}", ts);
         ts.extend(sig);
         assert_eq!(ts.len(), self.expected_len());
         Ok(ts)
     }
-    pub fn verify(&self, sig: Vec<u8>, public_key: &PublicKey) -> Result<()> {
-        // remove ts
-        // let (msg, sig) = input.split_at(8);
-        let msg = u64_to_bytes(self.0);
-        self.verify_message(&msg.to_vec(), &sig.to_vec(), public_key)
+    /// Verify signed token
+    pub fn verify(&self, public_key: &PublicKey) -> Result<()> {
+        if let None = self.1 {
+            return Err(anyhow!("no sig".to_string()));
+        }
+        let msg = u32_to_bytes(self.0);
+        self.verify_message(&msg.to_vec(), &self.1.unwrap(), public_key)
     }
-    /// Sign a Lightning message
-    pub fn sign_message(&self, message: &Vec<u8>, secret_key: &SecretKey) -> Result<Vec<u8>> {
+    /// Recover pubkey from signed token
+    pub fn recover(&self) -> Result<PublicKey> {
+        if let None = self.1 {
+            return Err(anyhow!("no sig".to_string()));
+        }
+        let msg = u32_to_bytes(self.0);
+        self.recover_pubkey(&msg.to_vec(), &self.1.unwrap())
+    }
+    /// Recover pubkey from signed token, and check timestamp
+    pub fn recover_within(&self, secs: u32) -> Result<PublicKey> {
+        if let None = self.1 {
+            return Err(anyhow!("no sig".to_string()));
+        }
+        if self.0 < now() - secs {
+            return Err(anyhow!("expired".to_string()));
+        }
+        let msg = u32_to_bytes(self.0);
+        self.recover_pubkey(&msg.to_vec(), &self.1.unwrap())
+    }
+    fn sign_message(&self, message: &Vec<u8>, secret_key: &SecretKey) -> Result<Vec<u8>> {
         let encmsg = self.lightning_hash(message)?;
         let secp_ctx = Secp256k1::signing_only();
         let sig = secp_ctx.sign_ecdsa_recoverable(&encmsg, &secret_key);
@@ -75,11 +98,10 @@ impl Token {
         res.push(rid.to_i32() as u8);
         Ok(res)
     }
-    /// Verify a Lightning message
-    pub fn verify_message(
+    fn verify_message(
         &self,
         message: &Vec<u8>,
-        sig: &Vec<u8>,
+        sig: &[u8; 65],
         public_key: &PublicKey,
     ) -> Result<()> {
         let secp_ctx = Secp256k1::verification_only();
@@ -89,8 +111,17 @@ impl Token {
         secp_ctx.verify_ecdsa(&encmsg, &s, public_key)?;
         Ok(())
     }
-    /// hash lightning message
-    pub fn lightning_hash(&self, message: &Vec<u8>) -> Result<Message> {
+    fn recover_pubkey(&self, message: &Vec<u8>, sig: &[u8; 65]) -> Result<PublicKey> {
+        if sig.len() < 65 {
+            return Err(anyhow!("too short sig".to_string()));
+        }
+        let encmsg = self.lightning_hash(message)?;
+        let secp = Secp256k1::verification_only();
+        let id = ecdsa::RecoveryId::from_i32(sig[64] as i32)?;
+        let s = ecdsa::RecoverableSignature::from_compact(&sig[..64], id)?;
+        Ok(secp.recover_ecdsa(&encmsg, &s)?)
+    }
+    fn lightning_hash(&self, message: &Vec<u8>) -> Result<Message> {
         let mut buffer = String::from("Lightning Signed Message:").into_bytes();
         buffer.extend(message);
         let hash = Sha256dHash::hash(&buffer);
@@ -122,6 +153,10 @@ mod tests {
         SecretKey::from_slice(&[0xcd; 32]).expect("32 bytes, within curve order")
     }
 
+    fn premade_token() -> String {
+        "7K3uYhxpP-qScsGcSXjBLsBMJ7rGf6mJd4ZPLbic80xmbyUFWpe4XtjbA3cFf08LTDH0ahd0UpOJFaZscFiptZQLaNIB".to_string()
+    }
+
     #[test]
     fn test_sign() {
         let secp = Secp256k1::new();
@@ -136,12 +171,14 @@ mod tests {
     #[test]
     fn test_token() {
         let sk = secret_key();
-        let t = Token::new();
+        let mut t = Token::new();
         let res = t.sign(&sk).expect("couldnt make token");
+        // println!("{}", base64_encode(&res));
         let secp = Secp256k1::new();
         let public_key = PublicKey::from_secret_key(&secp, &sk);
-        let (_, sig) = res.split_at(8);
-        t.verify(sig.to_vec(), &public_key).expect("couldnt verify");
+        let sig: [u8; 65] = res[4..].try_into().expect("wrong sig length");
+        t.set_sig(sig);
+        t.verify(&public_key).expect("couldnt verify");
         println!("token verified!");
     }
 
@@ -150,12 +187,46 @@ mod tests {
         let sk = secret_key();
         let secp = Secp256k1::new();
         let public_key = PublicKey::from_secret_key(&secp, &sk);
-        let s = "aHt45kxY9xZCMvT5du5mTw-jx3X2g0Eg7QhHTIi6rBRAFqY_syx1SzcSoriXyPIVCWdG6T0I8xKXSEnoeajFdwmtQbHqC_qfAQ==";
-        let v = base64_decode(s).expect("couldnt decode");
-        let (_, sig) = v.split_at(8);
-        let t = Token::from_base64(s).expect("couldnt parse base64");
-        t.verify(sig.to_vec(), &public_key)
-            .expect("failed to verify");
+        let t = Token::from_base64(&premade_token()).expect("couldnt parse base64");
+        t.verify(&public_key).expect("failed to verify");
         println!("decoded token verified!");
+    }
+
+    #[test]
+    fn test_recover() {
+        let sk = secret_key();
+        let secp = Secp256k1::new();
+        let public_key = PublicKey::from_secret_key(&secp, &sk);
+        let t = Token::from_base64(&premade_token()).expect("couldnt parse base64");
+        let pk2 = t.recover().expect("failed to verify");
+        assert_eq!(public_key, pk2);
+        println!("decoded token pubkey recovered!");
+    }
+
+    #[test]
+    fn test_recover_within() {
+        let sk = secret_key();
+        let t1 = Token::new();
+        let res = t1.sign(&sk).expect("couldnt make token");
+        let token = base64_encode(&res);
+        let secp = Secp256k1::new();
+        let public_key = PublicKey::from_secret_key(&secp, &sk);
+        let t = Token::from_base64(&token).expect("couldnt parse base64");
+        let pk2 = t.recover_within(10).expect("failed to verify");
+        assert_eq!(public_key, pk2);
+        println!("decoded token pubkey recovered!");
+    }
+
+    #[test]
+    fn test_check_timestamp() {
+        let sk = secret_key();
+        let t1 = Token::new();
+        let res = t1.sign(&sk).expect("couldnt make token");
+        let token = base64_encode(&res);
+        let t = Token::from_base64(&token).expect("couldnt parse base64");
+        std::thread::sleep(std::time::Duration::from_secs(2));
+        if t.recover_within(1).is_ok() {
+            panic!("should fail")
+        }
     }
 }
