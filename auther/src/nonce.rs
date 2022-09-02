e0721807cf7e471896a220899da903ecd6b07d6a
Evan Feenstra <evanfeenstra@gmail.com>
2022-09-02 14:34:12 -0700

    0.1.9 input args slice not vec

--- auther/src/nonce.rs
+++ auther/src/nonce.rs
@@ -12,33 +12,33 @@ impl Controller {
     pub fn new(sk: SecretKey, pk: PublicKey, nonce: u64) -> Self {
         Self(sk, pk, nonce)
     }
-    pub fn build_msg(&mut self, d: Vec<u8>) -> anyhow::Result<Vec<u8>> {
+    pub fn build_msg(&mut self, d: &[u8]) -> anyhow::Result<Vec<u8>> {
         self.2 = self.2 + 1;
         Ok(build_msg(d, &self.0, self.2)?)
     }
-    pub fn build_msg_with_nonce(&mut self, d: Vec<u8>, nonce: u64) -> anyhow::Result<Vec<u8>> {
+    pub fn build_msg_with_nonce(&mut self, d: &[u8], nonce: u64) -> anyhow::Result<Vec<u8>> {
         Ok(build_msg(d, &self.0, nonce)?)
     }
-    pub fn parse_msg(&mut self, input: Vec<u8>) -> anyhow::Result<Vec<u8>> {
+    pub fn parse_msg(&mut self, input: &[u8]) -> anyhow::Result<Vec<u8>> {
         let res = parse_msg(input, &self.1, self.2)?;
         self.2 = self.2 + 1;
         Ok(res)
     }
-    pub fn parse_msg_with_nonce(&mut self, input: Vec<u8>, nonce: u64) -> anyhow::Result<Vec<u8>> {
+    pub fn parse_msg_with_nonce(&mut self, input: &[u8], nonce: u64) -> anyhow::Result<Vec<u8>> {
         let res = parse_msg(input, &self.1, nonce)?;
         Ok(res)
     }
 }
 
-pub fn build_msg(input: Vec<u8>, sk: &SecretKey, nonce: u64) -> anyhow::Result<Vec<u8>> {
-    let mut d = input;
+pub fn build_msg(input: &[u8], sk: &SecretKey, nonce: u64) -> anyhow::Result<Vec<u8>> {
+    let mut d = input.to_vec();
     d.extend_from_slice(&nonce.to_be_bytes());
     let sig = sign_message(&d, sk)?;
     d.extend_from_slice(&sig);
     Ok(d)
 }
 
-pub fn parse_msg(input: Vec<u8>, pk: &PublicKey, last_nonce: u64) -> anyhow::Result<Vec<u8>> {
+pub fn parse_msg(input: &[u8], pk: &PublicKey, last_nonce: u64) -> anyhow::Result<Vec<u8>> {
     let msg_sig = input.split_at(input.len() - SIG_LEN);
     let sig: [u8; SIG_LEN] = msg_sig.1.try_into()?;
     let msg_nonce = msg_sig.0.split_at(msg_sig.0.len() - 8);
@@ -70,9 +70,9 @@ mod tests {
         let public_key = PublicKey::from_secret_key(&secp, &sk);
         let input = vec![1, 2, 3];
         let mut cont = Controller::new(sk, public_key, 0);
-        let msg = cont.build_msg(input.clone()).expect("couldnt sign");
+        let msg = cont.build_msg(&input).expect("couldnt sign");
         // 0 nonce, this is the first verification
-        let parsed = cont.parse_msg_with_nonce(msg, 0).expect("couldnt verify");
+        let parsed = cont.parse_msg_with_nonce(&msg, 0).expect("couldnt verify");
         assert_eq!(input, parsed, "unequal");
     }
 }
