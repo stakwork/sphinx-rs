bb044e9e8ce63475595f51de64405d560c5b62c6
Evan Feenstra <evanfeenstra@gmail.com>
2022-10-17 16:38:35 -0700

    demo control msgs

--- sphinx-wasm/demo/src/App.svelte
+++ sphinx-wasm/demo/src/App.svelte
@@ -7,6 +7,10 @@
     let sk = "86c8977989592a97beb409bc27fde76e981ce3543499fd61743755b832e92a3e";
     let pk = wasm.pubkey_from_secret_key(sk);
     console.log(pk);
+
+    let msg = { type: "Nonce" };
+    let req = wasm.build_control_request(JSON.stringify(msg), sk, BigInt(0));
+    console.log(req);
   }
   onMount(loadWasm);
 </script>
--- sphinx-wasm/src/lib.rs
+++ sphinx-wasm/src/lib.rs
@@ -5,3 +5,8 @@ use wasm_bindgen::prelude::*;
 pub fn pubkey_from_secret_key(sec: &str) -> String {
     cy::pubkey_from_secret_key(sec.to_string()).unwrap_or_default()
 }
+
+#[wasm_bindgen]
+pub fn build_control_request(msg: &str, secret: &str, nonce: u64) -> String {
+    cy::build_request(msg.to_string(), secret.to_string(), nonce).unwrap_or_default()
+}
