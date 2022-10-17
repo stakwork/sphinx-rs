199237b52d79a8cf59d2a20af9290715d7d8b520
Evan Feenstra <evanfeenstra@gmail.com>
2022-10-17 16:22:11 -0700

    wasm sphinx

---
+++ sphinx-wasm/demo/src/App.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import * as wasm from "../../pkg";
+  import { onMount } from "svelte";
+
+  async function loadWasm() {
+    await wasm.default("/sphinx_wasm_bg.wasm");
+    let sk = "86c8977989592a97beb409bc27fde76e981ce3543499fd61743755b832e92a3e";
+    let pk = wasm.pubkey_from_secret_key(sk);
+    console.log(pk);
+  }
+  onMount(loadWasm);
+</script>
+
+<main />
+
+<style>
+  main {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    background: black;
+  }
+</style>
--- sphinx-wasm/readme.md
+++ sphinx-wasm/readme.md
@@ -1,3 +1,11 @@
+### build
+
 comment out the [lib] section in ../sphinx-ffi/Cargo.toml
 
-wasm-pack build --target web
+AR=/usr/local/opt/llvm/bin/llvm-ar CC=/usr/local/opt/llvm/bin/clang wasm-pack build --target web
+
+cp pkg/sphinx_wasm_bg.wasm demo/public/sphinx_wasm_bg.wasm
+
+### dependencies
+
+brew install llvm
