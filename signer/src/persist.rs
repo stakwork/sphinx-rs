13585e7d814f8718fe64a5865a45f7e2daf370c6
decentclock <decentclock.5uh2k@slmail.me>
2022-12-02 11:09:52 -0500

    signer: update return types of persist trait methods

--- signer/src/persist.rs
+++ signer/src/persist.rs
@@ -15,6 +15,8 @@ use vls_persist::model::{
     AllowlistItemEntry, ChainTrackerEntry, ChannelEntry, NodeEntry, NodeStateEntry,
 };
 use vls_protocol_signer::lightning_signer;
+use vls_protocol_signer::lightning_signer::persist::Error;
+
 pub struct FsPersister {
     nodes: Bucket<NodeEntry>,
     states: Bucket<NodeStateEntry>,
@@ -46,7 +48,7 @@ fn get_channel_key(channel_id: &[u8]) -> &[u8] {
 }
 
 impl Persist for FsPersister {
-    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &CoreNodeState) {
+    fn new_node(&self, node_id: &PublicKey, config: &NodeConfig, state: &CoreNodeState) -> Result<(), Error> {
         let pk = hex::encode(node_id.serialize());
         let state_entry = state.into();
         let _ = self.states.put(&pk, state_entry);
@@ -56,21 +58,23 @@ impl Persist for FsPersister {
         };
         let _ = self.nodes.put(&pk, node_entry);
         let _ = self.pubkeys.put(&pk, node_id.clone());
+        Ok(())
     }
-    fn update_node(&self, node_id: &PublicKey, state: &CoreNodeState) -> Result<(), ()> {
+    fn update_node(&self, node_id: &PublicKey, state: &CoreNodeState) -> Result<(), Error> {
         let pk = hex::encode(node_id.serialize());
         let state_entry = state.into();
         let _ = self.states.put(&pk, state_entry);
         Ok(())
     }
-    fn delete_node(&self, node_id: &PublicKey) {
+    fn delete_node(&self, node_id: &PublicKey) -> Result<(), Error> {
         let pk = hex::encode(node_id.serialize());
         // clear all channel entries within "pk" sub-bucket
         let _ = self.channels.clear(&pk);
         let _ = self.nodes.remove(&pk);
         let _ = self.pubkeys.remove(&pk);
+        Ok(())
     }
-    fn new_channel(&self, node_id: &PublicKey, stub: &ChannelStub) -> Result<(), ()> {
+    fn new_channel(&self, node_id: &PublicKey, stub: &ChannelStub) -> Result<(), Error> {
         let pk = hex::encode(node_id.serialize());
         let chan_id = hex::encode(get_channel_key(stub.id0.as_slice()));
         // this breaks things...
@@ -87,33 +91,34 @@ impl Persist for FsPersister {
         let _ = self.channels.put(&pk, &chan_id, entry);
         Ok(())
     }
-    fn new_chain_tracker(&self, node_id: &PublicKey, tracker: &ChainTracker<ChainMonitor>) {
+    fn new_chain_tracker(&self, node_id: &PublicKey, tracker: &ChainTracker<ChainMonitor>) -> Result<(), Error> {
         let pk = hex::encode(node_id.serialize());
         let _ = self.chaintracker.put(&pk, tracker.into());
+        Ok(())
     }
     fn update_tracker(
         &self,
         node_id: &PublicKey,
         tracker: &ChainTracker<ChainMonitor>,
-    ) -> Result<(), ()> {
+    ) -> Result<(), Error> {
         log::info!("=> update_tracker");
         let pk = hex::encode(node_id.serialize());
         let _ = self.chaintracker.put(&pk, tracker.into());
         log::info!("=> update_tracker complete");
         Ok(())
     }
-    fn get_tracker(&self, node_id: &PublicKey) -> Result<ChainTracker<ChainMonitor>, ()> {
+    fn get_tracker(&self, node_id: &PublicKey) -> Result<ChainTracker<ChainMonitor>, Error> {
         let pk = hex::encode(node_id.serialize());
         let ret: ChainTrackerEntry = match self.chaintracker.get(&pk) {
             Ok(ct) => ct,
             Err(_) => {
                 log::error!("persister: failed to get_tracker");
-                return Err(());
+                return Err(Error::NotFound("Failed on get_tracker".to_string()));
             }
         };
         Ok(ret.into())
     }
-    fn update_channel(&self, node_id: &PublicKey, channel: &Channel) -> Result<(), ()> {
+    fn update_channel(&self, node_id: &PublicKey, channel: &Channel) -> Result<(), Error> {
         log::info!("=> update_channel");
         let pk = hex::encode(node_id.serialize());
         let chan_id = hex::encode(get_channel_key(channel.id0.as_slice()));
@@ -136,24 +141,24 @@ impl Persist for FsPersister {
         &self,
         node_id: &PublicKey,
         channel_id: &ChannelId,
-    ) -> Result<CoreChannelEntry, ()> {
+    ) -> Result<CoreChannelEntry, Error> {
         let pk = hex::encode(node_id.serialize());
         let chan_id = hex::encode(get_channel_key(channel_id.as_slice()));
         let ret: ChannelEntry = match self.channels.get(&pk, &chan_id) {
             Ok(ce) => ce,
             Err(_) => {
                 log::error!("persister: failed to get_channel");
-                return Err(());
+                return Err(Error::NotFound("Failed on get_channel".to_string()));
             }
         };
         Ok(ret.into())
     }
-    fn get_node_channels(&self, node_id: &PublicKey) -> Vec<(ChannelId, CoreChannelEntry)> {
+    fn get_node_channels(&self, node_id: &PublicKey) -> Result<Vec<(ChannelId, CoreChannelEntry)>, Error> {
         let mut res = Vec::new();
         let pk = hex::encode(node_id.serialize());
         let list = match self.channels.list(&pk) {
             Ok(l) => l,
-            Err(_) => return res,
+            Err(_) => return Ok(res),
         };
         for channel in list {
             if let Ok(entry) = self.channels.get(&pk, &channel) {
@@ -161,27 +166,27 @@ impl Persist for FsPersister {
                 res.push((id, entry.into()));
             };
         }
-        res
+        Ok(res)
     }
-    fn update_node_allowlist(&self, node_id: &PublicKey, allowlist: Vec<String>) -> Result<(), ()> {
+    fn update_node_allowlist(&self, node_id: &PublicKey, allowlist: Vec<String>) -> Result<(), Error> {
         let pk = hex::encode(node_id.serialize());
         let entry = AllowlistItemEntry { allowlist };
         let _ = self.allowlist.put(&pk, entry);
         Ok(())
     }
-    fn get_node_allowlist(&self, node_id: &PublicKey) -> Vec<String> {
+    fn get_node_allowlist(&self, node_id: &PublicKey) -> Result<Vec<String>, Error> {
         let pk = hex::encode(node_id.serialize());
         let entry: AllowlistItemEntry = match self.allowlist.get(&pk) {
             Ok(e) => e,
-            Err(_) => return Vec::new(),
+            Err(_) => return Ok(Vec::new()),
         };
-        entry.allowlist
+        Ok(entry.allowlist)
     }
-    fn get_nodes(&self) -> Vec<(PublicKey, CoreNodeEntry)> {
+    fn get_nodes(&self) -> Result<Vec<(PublicKey, CoreNodeEntry)>, Error> {
         let mut res = Vec::new();
         let list = match self.nodes.list() {
             Ok(ns) => ns,
-            Err(_) => return res,
+            Err(_) => return Ok(res),
         };
         log::info!("NODE LIST LEN {}", list.len());
         for pk in list {
@@ -206,13 +211,14 @@ impl Persist for FsPersister {
                 }
             }
         }
-        res
+        Ok(res)
     }
-    fn clear_database(&self) {
+    fn clear_database(&self) -> Result<(), Error> {
         let _ = self.nodes.clear();
         let _ = self.channels.clear_all();
         let _ = self.allowlist.clear();
         let _ = self.chaintracker.clear();
         let _ = self.pubkeys.clear();
+        Ok(())
     }
 }
