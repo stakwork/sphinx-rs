0c04f4c36e802af5929d0757dacc728bb8d0fcf3
Evan Feenstra <evanfeenstra@gmail.com>
2023-07-06 16:55:52 -0700

    mobile signer vls/lss steps, start ffi bindings

--- sphinx-ffi/src/signer.rs
+++ sphinx-ffi/src/signer.rs
@@ -1,1 +1,8 @@
-pub fn hi() {}
+use crate::Result;
+use sphinx_signer::mobile::RunReturn;
+
+pub type VlsResult = RunReturn;
+
+pub fn run_init_1(args: String, msg1: Vec<u8>) -> Result<Vec<u8>> {
+    Ok(vec![])
+}
