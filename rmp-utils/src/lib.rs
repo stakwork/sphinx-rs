daab445e481d6a2be88c467a8419b3afe2df176f
irriden <irriden@proton.me>
2023-07-13 19:05:55 +0000

    rmp-utils: return results from functions
    a closure is specified where the error would otherwise be
    `ValueReadError<BytesReadError>` doesn't implement `std::fmt::Display`

--- rmp-utils/src/lib.rs
+++ rmp-utils/src/lib.rs
@@ -2,75 +2,88 @@ extern crate alloc;
 use alloc::collections::BTreeMap;
 use alloc::string::String;
 use alloc::vec::Vec;
+use anyhow::Error;
+use anyhow::Result;
 use rmp::{decode, encode};
 
-pub fn serialize_state_vec(v: &Vec<(String, (u64, Vec<u8>))>) -> Vec<u8> {
+pub fn serialize_state_vec(v: &Vec<(String, (u64, Vec<u8>))>) -> Result<Vec<u8>> {
     let mut buff = encode::buffer::ByteBuf::new();
-    encode::write_array_len(&mut buff, v.len() as u32).unwrap();
-    v.iter().for_each(|(x, (y, z))| {
-        encode::write_array_len(&mut buff, 2).unwrap();
-        serialize_state_element(&mut buff, x, y, z);
-    });
-    buff.into_vec()
+    encode::write_array_len(&mut buff, v.len() as u32).map_err(Error::msg)?;
+    for (x, (y, z)) in v {
+        encode::write_array_len(&mut buff, 2).map_err(Error::msg)?;
+        serialize_state_element(&mut buff, x, y, z)?;
+    }
+    Ok(buff.into_vec())
 }
 
-pub fn deserialize_state_vec(b: &[u8]) -> Vec<(String, (u64, Vec<u8>))> {
+pub fn deserialize_state_vec(b: &[u8]) -> Result<Vec<(String, (u64, Vec<u8>))>> {
     let mut bytes = decode::bytes::Bytes::new(b);
-    let length = decode::read_array_len(&mut bytes).unwrap();
+    let length = decode::read_array_len(&mut bytes)
+        .map_err(|_| Error::msg("could not read array length"))?;
     let mut object: Vec<(String, (u64, Vec<u8>))> = Vec::with_capacity(length as usize);
-    (0..length).for_each(|_| {
-        let _ = decode::read_array_len(&mut bytes).unwrap();
-        let (x, (y, z)) = deserialize_state_element(&mut bytes);
+    for _ in 0..length {
+        let _ = decode::read_array_len(&mut bytes)
+            .map_err(|_| Error::msg("could not read array length"))?;
+        let (x, (y, z)) = deserialize_state_element(&mut bytes)?;
         object.push((x, (y, z)));
-    });
-    object
+    }
+    Ok(object)
 }
 
-pub fn serialize_state_map(map: &BTreeMap<String, (u64, Vec<u8>)>) -> Vec<u8> {
+pub fn serialize_state_map(map: &BTreeMap<String, (u64, Vec<u8>)>) -> Result<Vec<u8>> {
     let mut buff = encode::buffer::ByteBuf::new();
-    encode::write_map_len(&mut buff, map.len() as u32).unwrap();
-    map.iter().for_each(|(x, (y, z))| {
-        serialize_state_element(&mut buff, x, y, z);
-    });
-    buff.into_vec()
+    encode::write_map_len(&mut buff, map.len() as u32).map_err(Error::msg)?;
+    for (x, (y, z)) in map {
+        serialize_state_element(&mut buff, x, y, z)?;
+    }
+    Ok(buff.into_vec())
 }
 
-pub fn deserialize_state_map(b: &[u8]) -> BTreeMap<String, (u64, Vec<u8>)> {
+pub fn deserialize_state_map(b: &[u8]) -> Result<BTreeMap<String, (u64, Vec<u8>)>> {
     let mut bytes = decode::bytes::Bytes::new(b);
-    let length = decode::read_map_len(&mut bytes).unwrap();
+    let length =
+        decode::read_map_len(&mut bytes).map_err(|_| Error::msg("could not read map length"))?;
     let mut object: BTreeMap<String, (u64, Vec<u8>)> = BTreeMap::new();
-    (0..length).for_each(|_| {
-        let (x, (y, z)) = deserialize_state_element(&mut bytes);
+    for _ in 0..length {
+        let (x, (y, z)) = deserialize_state_element(&mut bytes)?;
         object.insert(x, (y, z));
-    });
-    object
+    }
+    Ok(object)
 }
 
-fn serialize_state_element(buff: &mut encode::buffer::ByteBuf, x: &String, y: &u64, z: &Vec<u8>) {
-    encode::write_str(buff, x).unwrap();
-    encode::write_array_len(buff, 2).unwrap();
-    encode::write_uint(buff, *y).unwrap();
-    encode::write_array_len(buff, z.len() as u32).unwrap();
-    z.iter().for_each(|x| {
-        encode::write_uint(buff, *x as u64).unwrap();
-    });
+fn serialize_state_element(
+    buff: &mut encode::buffer::ByteBuf,
+    x: &String,
+    y: &u64,
+    z: &Vec<u8>,
+) -> Result<()> {
+    encode::write_str(buff, x).map_err(Error::msg)?;
+    encode::write_array_len(buff, 2).map_err(Error::msg)?;
+    encode::write_uint(buff, *y).map_err(Error::msg)?;
+    encode::write_array_len(buff, z.len() as u32).map_err(Error::msg)?;
+    for x in z {
+        encode::write_uint(buff, *x as u64).map_err(Error::msg)?;
+    }
+    Ok(())
 }
 
-fn deserialize_state_element(bytes: &mut decode::bytes::Bytes) -> (String, (u64, Vec<u8>)) {
+fn deserialize_state_element(bytes: &mut decode::bytes::Bytes) -> Result<(String, (u64, Vec<u8>))> {
     let mut temp = decode::bytes::Bytes::new(bytes.remaining_slice());
-    let length = decode::read_str_len(&mut temp).unwrap();
+    let length =
+        decode::read_str_len(&mut temp).map_err(|_| Error::msg("could not read str length"))?;
     let mut buff = vec![0u8; length as usize];
-    decode::read_str(bytes, &mut buff).unwrap();
-    let x = String::from_utf8(buff).unwrap();
-    let _ = decode::read_array_len(bytes).unwrap();
-    let y: u64 = decode::read_int(bytes).unwrap();
-    let length = decode::read_array_len(bytes).unwrap();
+    decode::read_str(bytes, &mut buff).map_err(|_| Error::msg("could not read str"))?;
+    let x = String::from_utf8(buff).map_err(Error::msg)?;
+    let _ = decode::read_array_len(bytes).map_err(|_| Error::msg("could not read array length"))?;
+    let y: u64 = decode::read_int(bytes).map_err(Error::msg)?;
+    let length =
+        decode::read_array_len(bytes).map_err(|_| Error::msg("could not read array length"))?;
     let mut z: Vec<u8> = Vec::with_capacity(length as usize);
-    (0..length).for_each(|_| {
-        let byte: u8 = decode::read_int(bytes).unwrap();
+    for _ in 0..length {
+        let byte: u8 = decode::read_int(bytes).map_err(Error::msg)?;
         z.push(byte);
-    });
-    (x, (y, z))
+    }
+    Ok((x, (y, z)))
 }
 
 #[test]
@@ -80,7 +93,7 @@ fn state_vec_serialize() {
         ("bbbb".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
         ("cccc".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
     ];
-    let buff = serialize_state_vec(&test);
+    let buff = serialize_state_vec(&test).unwrap();
     let reference = rmp_serde::to_vec(&test).unwrap();
     // Test bytes equality
     assert_eq!(reference, buff);
@@ -97,7 +110,7 @@ fn state_vec_deserialize() {
         ("cccc".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
     ];
     let bytes = rmp_serde::to_vec(&test).unwrap();
-    let object = deserialize_state_vec(&bytes[..]);
+    let object = deserialize_state_vec(&bytes[..]).unwrap();
     assert_eq!(test, object);
 }
 
@@ -107,7 +120,7 @@ fn state_map_serialize() {
     test.insert("aaaa".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
     test.insert("bbbb".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
     test.insert("cccc".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
-    let buff = serialize_state_map(&test);
+    let buff = serialize_state_map(&test).unwrap();
     let reference = rmp_serde::to_vec(&test).unwrap();
     // Test bytes equality
     assert_eq!(reference, buff);
@@ -123,6 +136,6 @@ fn state_map_deserialize() {
     test.insert("bbbb".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
     test.insert("cccc".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
     let bytes = rmp_serde::to_vec(&test).unwrap();
-    let object = deserialize_state_map(&bytes[..]);
+    let object = deserialize_state_map(&bytes[..]).unwrap();
     assert_eq!(test, object);
 }
