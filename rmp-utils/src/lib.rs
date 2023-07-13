e212167a634d4ab021187c2e7a6e2b4ea0b9758d
irriden <irriden@proton.me>
2023-07-13 22:13:35 +0000

    rmp-utils: write muts as bin array

--- rmp-utils/src/lib.rs
+++ rmp-utils/src/lib.rs
@@ -4,7 +4,10 @@ use alloc::string::String;
 use alloc::vec::Vec;
 use anyhow::Error;
 use anyhow::Result;
-use rmp::{decode, encode};
+use rmp::{
+    decode::{self, RmpRead},
+    encode,
+};
 
 pub fn serialize_state_vec(v: &Vec<(String, (u64, Vec<u8>))>) -> Result<Vec<u8>> {
     let mut buff = encode::buffer::ByteBuf::new();
@@ -60,10 +63,7 @@ fn serialize_state_element(
     encode::write_str(buff, x).map_err(Error::msg)?;
     encode::write_array_len(buff, 2).map_err(Error::msg)?;
     encode::write_uint(buff, *y).map_err(Error::msg)?;
-    encode::write_array_len(buff, z.len() as u32).map_err(Error::msg)?;
-    for x in z {
-        encode::write_uint(buff, *x as u64).map_err(Error::msg)?;
-    }
+    encode::write_bin(buff, &z).map_err(Error::msg)?;
     Ok(())
 }
 
@@ -77,65 +77,31 @@ fn deserialize_state_element(bytes: &mut decode::bytes::Bytes) -> Result<(String
     let _ = decode::read_array_len(bytes).map_err(|_| Error::msg("could not read array length"))?;
     let y: u64 = decode::read_int(bytes).map_err(Error::msg)?;
     let length =
-        decode::read_array_len(bytes).map_err(|_| Error::msg("could not read array length"))?;
-    let mut z: Vec<u8> = Vec::with_capacity(length as usize);
-    for _ in 0..length {
-        let byte: u8 = decode::read_int(bytes).map_err(Error::msg)?;
-        z.push(byte);
-    }
+        decode::read_bin_len(bytes).map_err(|_| Error::msg("could not read bin length"))?;
+    let mut z: Vec<u8> = vec![0u8; length as usize];
+    bytes.read_exact_buf(&mut z).map_err(Error::msg)?;
     Ok((x, (y, z)))
 }
 
 #[test]
-fn state_vec_serialize() {
+fn state_vec_serde() {
     let test = vec![
         ("aaaa".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
         ("bbbb".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
         ("cccc".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
     ];
-    let buff = serialize_state_vec(&test).unwrap();
-    let reference = rmp_serde::to_vec(&test).unwrap();
-    // Test bytes equality
-    assert_eq!(reference, buff);
-    // Deserialize, and check both objects are the same
-    let result: Vec<(String, (u64, Vec<u8>))> = rmp_serde::from_slice(buff.as_slice()).unwrap();
-    assert_eq!(test, result);
-}
-
-#[test]
-fn state_vec_deserialize() {
-    let test = vec![
-        ("aaaa".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
-        ("bbbb".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
-        ("cccc".to_string(), (15, vec![u8::MAX, u8::MAX, u8::MAX])),
-    ];
-    let bytes = rmp_serde::to_vec(&test).unwrap();
-    let object = deserialize_state_vec(&bytes[..]).unwrap();
+    let bytes = serialize_state_vec(&test).unwrap();
+    let object = deserialize_state_vec(&bytes).unwrap();
     assert_eq!(test, object);
 }
 
 #[test]
-fn state_map_serialize() {
+fn state_map_serde() {
     let mut test = BTreeMap::new();
     test.insert("aaaa".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
     test.insert("bbbb".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
     test.insert("cccc".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
-    let buff = serialize_state_map(&test).unwrap();
-    let reference = rmp_serde::to_vec(&test).unwrap();
-    // Test bytes equality
-    assert_eq!(reference, buff);
-    // Deserialize, and check both objects are the same
-    let result: BTreeMap<String, (u64, Vec<u8>)> = rmp_serde::from_slice(buff.as_slice()).unwrap();
-    assert_eq!(test, result);
-}
-
-#[test]
-fn state_map_deserialize() {
-    let mut test = BTreeMap::new();
-    test.insert("aaaa".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
-    test.insert("bbbb".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
-    test.insert("cccc".to_string(), (0, vec![u8::MAX, u8::MAX, u8::MAX]));
-    let bytes = rmp_serde::to_vec(&test).unwrap();
-    let object = deserialize_state_map(&bytes[..]).unwrap();
+    let bytes = serialize_state_map(&test).unwrap();
+    let object = deserialize_state_map(&bytes).unwrap();
     assert_eq!(test, object);
 }
