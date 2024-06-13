33dae2f89a00e82a81db118e6ac97327a828db54
Evan Feenstra <evanfeenstra@gmail.com>
2024-06-12 17:14:44 -0700

    bump sphinx, fix asyncpay publishing

--- sphinx-ffi/src/uniffi/sphinxrs/sphinxrs.kt
+++ sphinx-ffi/src/uniffi/sphinxrs/sphinxrs.kt
@@ -1223,6 +1223,8 @@ data class RunReturn (
     var `subscriptionTopics`: List<String>, 
     var `settleTopic`: String?, 
     var `settlePayload`: ByteArray?, 
+    var `asyncpayTopic`: String?, 
+    var `asyncpayPayload`: ByteArray?, 
     var `topics`: List<String>, 
     var `payloads`: List<ByteArray>, 
     var `stateMp`: ByteArray?, 
@@ -1231,6 +1233,7 @@ data class RunReturn (
     var `myContactInfo`: String?, 
     var `sentStatus`: String?, 
     var `settledStatus`: String?, 
+    var `asyncpayTag`: String?, 
     var `error`: String?, 
     var `newTribe`: String?, 
     var `tribeMembers`: String?, 
@@ -1261,11 +1264,14 @@ public object FfiConverterTypeRunReturn: FfiConverterRustBuffer<RunReturn> {
             FfiConverterSequenceString.read(buf),
             FfiConverterOptionalString.read(buf),
             FfiConverterOptionalByteArray.read(buf),
+            FfiConverterOptionalString.read(buf),
+            FfiConverterOptionalByteArray.read(buf),
             FfiConverterSequenceString.read(buf),
             FfiConverterSequenceByteArray.read(buf),
             FfiConverterOptionalByteArray.read(buf),
             FfiConverterSequenceString.read(buf),
             FfiConverterOptionalULong.read(buf),
+            FfiConverterOptionalString.read(buf),
             FfiConverterOptionalString.read(buf),
             FfiConverterOptionalString.read(buf),
             FfiConverterOptionalString.read(buf),
@@ -1296,6 +1302,8 @@ public object FfiConverterTypeRunReturn: FfiConverterRustBuffer<RunReturn> {
             FfiConverterSequenceString.allocationSize(value.`subscriptionTopics`) +
             FfiConverterOptionalString.allocationSize(value.`settleTopic`) +
             FfiConverterOptionalByteArray.allocationSize(value.`settlePayload`) +
+            FfiConverterOptionalString.allocationSize(value.`asyncpayTopic`) +
+            FfiConverterOptionalByteArray.allocationSize(value.`asyncpayPayload`) +
             FfiConverterSequenceString.allocationSize(value.`topics`) +
             FfiConverterSequenceByteArray.allocationSize(value.`payloads`) +
             FfiConverterOptionalByteArray.allocationSize(value.`stateMp`) +
@@ -1304,6 +1312,7 @@ public object FfiConverterTypeRunReturn: FfiConverterRustBuffer<RunReturn> {
             FfiConverterOptionalString.allocationSize(value.`myContactInfo`) +
             FfiConverterOptionalString.allocationSize(value.`sentStatus`) +
             FfiConverterOptionalString.allocationSize(value.`settledStatus`) +
+            FfiConverterOptionalString.allocationSize(value.`asyncpayTag`) +
             FfiConverterOptionalString.allocationSize(value.`error`) +
             FfiConverterOptionalString.allocationSize(value.`newTribe`) +
             FfiConverterOptionalString.allocationSize(value.`tribeMembers`) +
@@ -1330,6 +1339,8 @@ public object FfiConverterTypeRunReturn: FfiConverterRustBuffer<RunReturn> {
             FfiConverterSequenceString.write(value.`subscriptionTopics`, buf)
             FfiConverterOptionalString.write(value.`settleTopic`, buf)
             FfiConverterOptionalByteArray.write(value.`settlePayload`, buf)
+            FfiConverterOptionalString.write(value.`asyncpayTopic`, buf)
+            FfiConverterOptionalByteArray.write(value.`asyncpayPayload`, buf)
             FfiConverterSequenceString.write(value.`topics`, buf)
             FfiConverterSequenceByteArray.write(value.`payloads`, buf)
             FfiConverterOptionalByteArray.write(value.`stateMp`, buf)
@@ -1338,6 +1349,7 @@ public object FfiConverterTypeRunReturn: FfiConverterRustBuffer<RunReturn> {
             FfiConverterOptionalString.write(value.`myContactInfo`, buf)
             FfiConverterOptionalString.write(value.`sentStatus`, buf)
             FfiConverterOptionalString.write(value.`settledStatus`, buf)
+            FfiConverterOptionalString.write(value.`asyncpayTag`, buf)
             FfiConverterOptionalString.write(value.`error`, buf)
             FfiConverterOptionalString.write(value.`newTribe`, buf)
             FfiConverterOptionalString.write(value.`tribeMembers`, buf)
